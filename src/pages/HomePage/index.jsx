d3b7a41feb3446665e41b5550f82658cd247c8eb
madhav-v <madhavdhungana36@gmail.com>
2023-12-15 18:20:27 +0545

    Home page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,21 +4,25 @@ const HomePage = () => {
   return (
     <>
       <div
-        className="bg-cover bg-center bg-no-repeat min-h-screen"
+        className="bg-cover bg-center bg-no-repeat min-h-screen relative"
         style={{ backgroundImage: `url(${background})` }}
       >
-        {" "}
         <div className="absolute inset-0 bg-black opacity-70"></div>
-        <div>
-          <div className="absolute inset-0 flex justify-center text-white">
-            <h1 className="text-4xl font-bold">Welcome to Bihe.com</h1>
-          </div>
-          <div>
-            <div className="absolute inset-0 mt-10 ml-10 flex flex-col text-white">
-              <NavLink to="/login">Login</NavLink>
-
-              <NavLink to="/register">Register</NavLink>
-            </div>
+        <div className="text-container absolute inset-0 flex flex-col items-center justify-center text-white">
+          <h1 className="text-4xl font-bold">Welcome to Bihe.com</h1>
+          <div className="link-container mt-10">
+            <NavLink
+              to="/login"
+              className="bg-blue-500 hover:bg-blue-700 text-white mr-5 font-bold py-2 px-4 rounded-full text-lg"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              to="/register"
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full text-lg mt-4"
+            >
+              Register
+            </NavLink>
           </div>
         </div>
       </div>
