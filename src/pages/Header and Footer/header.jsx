05d7c4d21ff792a70b69e46bf76ab3c04f52031a
madhav-v <madhavdhungana36@gmail.com>
2023-12-16 21:01:06 +0545

    Logout logiv

--- src/pages/Header and Footer/header.jsx
+++ src/pages/Header and Footer/header.jsx
@@ -1,24 +1,29 @@
 import React, { useRef } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import "../../App.css";
 import logo from "../../../public/logo.png";
 import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Header = () => {
   const sidenav = useRef();
   const sidenavblur = useRef();
   const loggedInUser = useSelector((state) => state.User.loggedInUser);
-
   const sidenavs = () => {
     sidenav.current.style.bottom = "0px";
     sidenavblur.current.style.bottom = "0px";
   };
-
+  const navigate = useNavigate();
   const sidenavss = () => {
     sidenav.current.style.bottom = "-100%";
     sidenavblur.current.style.bottom = "-100%";
   };
-
+  const logout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    toast.success("Thanks for using our service.");
+    navigate("/login");
+  };
   return (
     <>
       <header className="Navbar">
@@ -61,69 +66,12 @@ const Header = () => {
               </button>
             </Link>
           ) : (
-            <Link to="/">
+            <Link to="/user">
               <button className="nav-btn text-sm md:text-base text-white">
-                Logout
+                {loggedInUser.name.split(" ")[0]}{" "}
               </button>
             </Link>
           )}
-        </div>
-        <div className="sidenavbar lg:hidden">
-          <div ref={sidenav} className="sidenav">
-            <div className="snbar"></div>
-            <ul className="sidenavbar-items">
-              <li className="sidenavbar-item">
-                <NavLink
-                  className="navbar-link cool-link text-sm md:text-base"
-                  aria-current="page"
-                  to="/"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="sidenavbar-item">
-                <NavLink
-                  className="navbar-link cool-link text-sm md:text-base"
-                  to="/about"
-                >
-                  About Our Life Partner
-                </NavLink>
-              </li>
-              <li className="sidenavbar-item">
-                <NavLink
-                  className="navbar-link cool-link text-sm md:text-base"
-                  to="/help"
-                >
-                  Help?
-                </NavLink>
-              </li>
-            </ul>
-
-            <div>
-              {!loggedInUser ? (
-                <span className="sidenavbar-item ">
-                  <Link to="/signup">
-                    <button className="sidenav-btn text-sm md:text-base">
-                      Register Now
-                    </button>
-                  </Link>
-                </span>
-              ) : (
-                <span className="sidenavbar-item ">
-                  <Link to="/">
-                    <button className="sidenav-btn text-sm md:text-base">
-                      Logout
-                    </button>
-                  </Link>
-                </span>
-              )}
-            </div>
-          </div>
-          <div
-            ref={sidenavblur}
-            className="sidenavblur"
-            onClick={sidenavss}
-          ></div>
         </div>
       </header>
     </>
