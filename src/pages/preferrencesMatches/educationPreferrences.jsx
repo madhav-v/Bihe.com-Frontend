c3c9f9e02b9015d69001dec72634f412e2c3a0c6
madhav-v <madhavdhungana36@gmail.com>
2024-01-08 18:21:48 +0545

    Minor changes

--- src/pages/preferrencesMatches/educationPreferrences.jsx
+++ src/pages/preferrencesMatches/educationPreferrences.jsx
@@ -1,40 +1,52 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import preferrenceSvc from "../../services/preferreces.service";
 import RecommendItem from "../Dashboard/comp/recommenditem";
 import NavBar from "../../components/Navbar";
 import noData from "../../../public/3009287.jpg";
 
 const EducationPreferrence = () => {
-  const [educationInfo, setEducationInfo] = useState();
+  const [educationInfo, setEducationInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const educationDetails = async () => {
     try {
       const detail = await preferrenceSvc.getByEducation();
       setEducationInfo(detail.result);
+      setLoading(false);
     } catch (exception) {
+      setLoading(false);
       throw exception;
     }
   };
+
   useEffect(() => {
     const fetchData = async () => {
       await educationDetails();
     };
-    console.log("ed", educationInfo);
+
     fetchData();
-  }, [educationInfo]);
+  }, []);
+
   return (
     <>
-      <div className="mt-5 ml-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-4">
-        {educationInfo && educationInfo.length > 0 ? (
-          educationInfo.map((match, index) => (
-            <RecommendItem key={index} recommend={match} />
-          ))
-        ) : (
-          <div className="flex flex-col items-center justify-center mt-5">
-            <img src={noData} alt="No Matches Found" />
-            <p className="mt-3">No matches found</p>
-          </div>
-        )}
-      </div>
+      {loading ? (
+        <div className="flex flex-col items-center justify-center mt-[10vh]">
+          <p className="mt-3">Loading...</p>
+        </div>
+      ) : (
+        <div className="mt-5 ml-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-4">
+          {educationInfo && educationInfo.length > 0 ? (
+            educationInfo.map((match, index) => (
+              <RecommendItem key={index} recommend={match} />
+            ))
+          ) : (
+            <div className="flex flex-col items-center justify-center mt-5">
+              <img src={noData} alt="No Matches Found" />
+              <p className="mt-3">No matches found</p>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
--- src/pages/preferrencesMatches/occupationPreferrences.jsx
+++ src/pages/preferrencesMatches/occupationPreferrences.jsx
@@ -7,12 +7,15 @@ import noData from "../../../public/3009287.jpg";
 const OccupationPreferrence = () => {
   const [occupationInfo, setOccupationInfo] = useState();
   const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
   const occupationDetails = async () => {
     try {
       const detail = await preferrenceSvc.getByOccupation();
       setOccupationInfo(detail.result);
       setUser(detail.user);
+      setLoading(false);
     } catch (exception) {
+      setLoading(false);
       throw exception;
     }
   };
@@ -22,21 +25,27 @@ const OccupationPreferrence = () => {
     };
 
     fetchData();
-  }, [occupationInfo]);
+  }, []);
   return (
     <>
-      <div className="mt-5 ml-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-4">
-        {occupationInfo && occupationInfo.length > 0 ? (
-          occupationInfo.map((match, index) => (
-            <RecommendItem key={index} recommend={match} user={user[index]} />
-          ))
-        ) : (
-          <div className="flex flex-col items-center justify-center mt-5">
-            <img src={noData} alt="No Matches Found" />
-            <p className="mt-3">No matches found</p>
-          </div>
-        )}
-      </div>
+      {loading ? (
+        <div className="flex flex-col items-center justify-center mt-[10vh]">
+          <p className="mt-3">Loading...</p>
+        </div>
+      ) : (
+        <div className="mt-5 ml-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-4">
+          {occupationInfo && occupationInfo.length > 0 ? (
+            occupationInfo.map((match, index) => (
+              <RecommendItem key={index} recommend={match} user={user[index]} />
+            ))
+          ) : (
+            <div className="flex flex-col items-center justify-center mt-5">
+              <img src={noData} alt="No Matches Found" />
+              <p className="mt-3">No matches found</p>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
